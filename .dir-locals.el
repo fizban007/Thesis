13e5c1eab609dcfb72730ada686f87d76483f2ed
fizban007 <fizban007@gmail.com>
2017-01-26 17:18:25 -0500

    Added dir locals file

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((latex-mode
+  (reftex-default-bibliography "thesis.bib"))
+ (latex-mode
+  (zotero-collection #("18" 0 2 (name "Thesis")))))
